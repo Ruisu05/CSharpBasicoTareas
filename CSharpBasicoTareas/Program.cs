c6fdebbab16133a7a0ce374cc2349f83225d5f8f
Luis <alberty_filpo@hotmail.com>
2022-04-28 17:25:02 -0400

    Initial commit

---
+++ CSharpBasicoTareas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasicoTareas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
